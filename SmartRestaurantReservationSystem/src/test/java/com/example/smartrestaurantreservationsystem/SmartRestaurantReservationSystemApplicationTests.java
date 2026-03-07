d78ee2b3c98eab70152be44f4948c79a167733ab
alinavassiljeva <alinva@taltech.ee>
2026-03-07 23:21:04 +0200

    Empty project

---
+++ SmartRestaurantReservationSystem/src/test/java/com/example/smartrestaurantreservationsystem/SmartRestaurantReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartrestaurantreservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRestaurantReservationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
